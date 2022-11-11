4db155a9fb3b6347ebf3a95c8ac0b9504aeaf241
Martin Matusiak <2914422+Nithos@users.noreply.github.com>
2022-11-11 14:39:06 -0500

    Update kubernetes/local/deploy-terarium.sh
    Co-authored-by: Yohann Paris <github@yohannparis.com>

--- kubernetes/local/deploy-terarium.sh
+++ kubernetes/local/deploy-terarium.sh
@@ -63,7 +63,7 @@ if [[ ${1} == "dev" ]]; then
 fi
 
 echo "Usage:"
-echo "    ${0} up        launches the Gateway and Authentication services and hmi (i.e. full stack)"
-echo "    ${0} down      tears down the Gateway and Authentication services"
-echo "    ${0} dev       launches the Gateway and Authentication services"
+echo "    ${0} up        launches TERArium"
+echo "    ${0} down      tears down TERArium"
+echo "    ${0} dev       launches only the Gateway and Authentication services"
 echo "    ${0} status    displays the status of the Gateway and Authentication services"
