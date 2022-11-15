b72aadc5f95a367d8293601842cab47b8b3ce04c
Yohann Paris <github@yohannparis.com>
2022-11-15 16:22:58 -0500

    Update deploy-terarium.sh

--- kubernetes/local/deploy-terarium.sh
+++ kubernetes/local/deploy-terarium.sh
@@ -1,73 +1,68 @@
 #!/bin/bash
 
+function start_gateway() {
+    kubectl apply --filename 'gateway-*.yaml'
+}
 
+function start_hmi-server() {
+    # Wait for Keyclock, the hmi-server test its existence on start
+    kubectl rollout status --filename 'gateway-keycloak-*.yaml'
+    kubectl apply --filename 'hmi-server-*.yaml','mock-data-service-*.yaml'
+}
+
+function start_hmi-client() {
+    kubectl apply --filename 'hmi-client-*.yaml'
+}
+
+# launches TERArium
 if [[ ${1} == "up" ]]; then
-
-    kubectl apply \
-    -f gateway-postgres-service.yaml \
-    -f gateway-postgres-deployment.yaml \
-    -f gateway-keycloak-realm.yaml \
-    -f gateway-keycloak-service.yaml \
-    -f gateway-keycloak-deployment.yaml \
-    -f gateway-httpd-config.yaml \
-    -f gateway-httpd-htdocs.yaml \
-    -f gateway-httpd-service.yaml \
-    -f gateway-httpd-deployment.yaml \
-    -f hmi-server-service.yaml \
-    -f hmi-server-deployment.yaml \
-    -f hmi-client-service.yaml \
-    -f hmi-client-deployment.yaml \
-    -f mock-data-service-service.yaml \
-    -f mock-data-service-deployment.yaml
-
+    start_gateway && \
+    start_hmi-server && \
+    start_hmi-client
     exit 0
 fi
 
+# tears down TERArium
 if [[ ${1} == "down" ]]; then
     kubectl delete \
-    -f gateway-postgres-service.yaml \
-    -f gateway-postgres-deployment.yaml \
-    -f gateway-keycloak-realm.yaml \
-    -f gateway-keycloak-service.yaml \
-    -f gateway-keycloak-deployment.yaml \
-    -f gateway-httpd-config.yaml \
-    -f gateway-httpd-htdocs.yaml \
-    -f gateway-httpd-service.yaml \
-    -f gateway-httpd-deployment.yaml \
-    -f hmi-server-service.yaml \
-    -f hmi-server-deployment.yaml \
-    -f hmi-client-service.yaml \
-    -f hmi-client-deployment.yaml \
-    -f mock-data-service-service.yaml \
-    -f mock-data-service-deployment.yaml
-
+    --filename 'gateway-*.yaml' \
+    --filename 'hmi-server-*.yaml' \
+    --filename 'mock-data-service-*.yaml' \
+    --filename 'hmi-client-*.yaml'
     exit 0
 fi
 
+# displays the status of all services
 if [[ ${1} == "status" ]]; then
     kubectl get po,svc,configMap
-
     exit 0
 fi
 
+# launches only the Gateway and Authentication services
 if [[ ${1} == "dev" ]]; then
+    start_gateway
+    exit 0
+fi
 
-    kubectl apply \
-    -f gateway-postgres-service.yaml \
-    -f gateway-postgres-deployment.yaml \
-    -f gateway-keycloak-realm.yaml \
-    -f gateway-keycloak-service.yaml \
-    -f gateway-keycloak-deployment.yaml \
-    -f gateway-httpd-config.yaml \
-    -f gateway-httpd-htdocs.yaml \
-    -f gateway-httpd-service.yaml \
-    -f gateway-httpd-deployment.yaml
+# launches TERArium without hmi-server
+if [[ ${1} == "dev:hmi-server" ]]; then
+    start_gateway && \
+    start_hmi-client
+    exit 0
+fi
 
+# launches TERArium without hmi-client
+if [[ ${1} == "dev:hmi-client" ]]; then
+    start_gateway && \
+    start_hmi-server
     exit 0
 fi
 
 echo "Usage:"
-echo "    ${0} up        launches TERArium"
-echo "    ${0} down      tears down TERArium"
-echo "    ${0} dev       launches only the Gateway and Authentication services"
-echo "    ${0} status    displays the status of the Gateway and Authentication services"
+echo "    ${0} status            displays the status of all services"
+echo "    ${0} up                launches TERArium"
+echo "    ${0} down              tears down TERArium"
+echo "    ${0} dev               launches only the Gateway and Authentication services"
+echo "    ${0} dev:hmi-server    launches TERArium without hmi-server"
+echo "    ${0} dev:hmi-client    launches TERArium without hmi-client"
+
