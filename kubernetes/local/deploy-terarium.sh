1938a13230b6e45fc5ea546dcedcc5f2f873a124
David Gauldie <dgauldie@uncharted.software>
2022-12-01 15:44:49 -0500

    ci: add data service deployment scripts (#32)
    Co-authored-by: Yohann Paris <github@yohannparis.com>

--- kubernetes/local/deploy-terarium.sh
+++ kubernetes/local/deploy-terarium.sh
@@ -4,10 +4,22 @@ function start_gateway() {
     kubectl apply --filename 'gateway-*.yaml'
 }
 
+function start_db() {
+	kubectl apply --filename 'data-service-postgres-*.yaml'
+	kubectl apply --filename 'data-service-graphdb-*.yaml'
+}
+
+function start_data-service() {
+    # Wait for postgres and graphdb to start
+    kubectl rollout status --filename 'data-service-postgres-*.yaml'
+    kubectl rollout status --filename 'data-service-graphdb-*.yaml'
+    kubectl apply --filename 'data-service-deployment.yaml' --filename 'data-service-service.yaml'
+}
+
 function start_hmi-server() {
     # Wait for Keyclock, the hmi-server test its existence on start
     kubectl rollout status --filename 'gateway-keycloak-*.yaml'
-    kubectl apply --filename 'hmi-server-*.yaml','mock-data-service-*.yaml'
+    kubectl apply --filename 'hmi-server-*.yaml'
 }
 
 function start_hmi-client() {
@@ -23,6 +35,8 @@ fi
 # Launches TERArium
 if [[ ${1} == "up" ]]; then
     start_gateway && \
+    start_db && \
+    start_data-service && \
     start_hmi-server && \
     start_hmi-client
     exit 0
@@ -33,20 +47,24 @@ if [[ ${1} == "down" ]]; then
     kubectl delete \
     --filename 'gateway-*.yaml' \
     --filename 'hmi-server-*.yaml' \
-    --filename 'mock-data-service-*.yaml' \
+    --filename 'data-service-*.yaml' \
     --filename 'hmi-client-*.yaml'
     exit 0
 fi
 
 # Launches only the Gateway and Authentication services
 if [[ ${1} == "dev" ]]; then
-    start_gateway
+    start_gateway && \
+    start_db && \
+    start_data-service
     exit 0
 fi
 
 # Launches TERArium without the hmi-server
 if [[ ${1} == "dev:no-hmi-server" ]]; then
     start_gateway && \
+    start_db && \
+    start_data-service && \
     start_hmi-client
     exit 0
 fi
@@ -54,6 +72,8 @@ fi
 # Launches TERArium without the hmi-client
 if [[ ${1} == "dev:no-hmi-client" ]]; then
     start_gateway && \
+    start_db && \
+    start_data-service && \
     start_hmi-server
     exit 0
 fi
