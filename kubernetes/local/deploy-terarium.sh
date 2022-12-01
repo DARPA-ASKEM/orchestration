056413c402c5204dcc6207dc2643d368a1bcdfbe
dgauldie <dgauldie@uncharted.software>
2022-12-01 11:31:44 -0500

    fix: fis issues with deployment script

--- kubernetes/local/deploy-terarium.sh
+++ kubernetes/local/deploy-terarium.sh
@@ -4,10 +4,22 @@ function start_gateway() {
     kubectl apply --filename 'gateway-*.yaml'
 }
 
+
+
+function start_data-service() {
+    # Wait for postgres to start
+    kubectl apply --filename 'data-service-postgres-*.yaml'
+    kubectl rollout status --filename 'data-service-postgres-*.yaml'
+    # Wait for graphdb to start
+    kubectl apply --filename 'data-service-graphdb-*.yaml'
+    kubectl rollout status --filename 'data-service-graphdb-*.yaml'
+    kubectl apply --filename 'data-service-deployment.yaml' --filename 'data-service-service.yaml'
+}
+
 function start_hmi-server() {
     # Wait for Keyclock, the hmi-server test its existence on start
     kubectl rollout status --filename 'gateway-keycloak-*.yaml'
-    kubectl apply --filename 'hmi-server-*.yaml','data-service-*.yaml'
+    kubectl apply --filename 'hmi-server-*.yaml'
 }
 
 function start_hmi-client() {
@@ -23,6 +35,7 @@ fi
 # Launches TERArium
 if [[ ${1} == "up" ]]; then
     start_gateway && \
+    start_data-service && \
     start_hmi-server && \
     start_hmi-client
     exit 0
@@ -40,13 +53,15 @@ fi
 
 # Launches only the Gateway and Authentication services
 if [[ ${1} == "dev" ]]; then
-    start_gateway
+    start_gateway && \
+    start_data-service
     exit 0
 fi
 
 # Launches TERArium without the hmi-server
 if [[ ${1} == "dev:no-hmi-server" ]]; then
     start_gateway && \
+    start_data-service && \
     start_hmi-client
     exit 0
 fi
@@ -54,6 +69,7 @@ fi
 # Launches TERArium without the hmi-client
 if [[ ${1} == "dev:no-hmi-client" ]]; then
     start_gateway && \
+    start_data-service && \
     start_hmi-server
     exit 0
 fi
