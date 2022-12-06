3d88f66b87e0826880b45ed798280b54817ad8df
Charles Coleman <bigglesandginger@gmail.com>
2022-12-06 16:25:00 -0500

    remove encryption from kubernetes configuration files, only encrypt secrets

--- kubernetes/staging/deploy-terarium.sh
+++ kubernetes/staging/deploy-terarium.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
+decrypt() {
+    ansible-vault decrypt --vault-id ~/askem-vault-id.txt secrets*.yaml
+    ansible-vault decrypt --vault-id ~/askem-vault-id.txt gateway/keycloak/certificates/*.pem
+    ansible-vault decrypt --vault-id ~/askem-vault-id.txt gateway/keycloak/realm/*.json
+}
+
+encrypt() {
+    ansible-vault encrypt --vault-id ~/askem-vault-id.txt secrets*.yaml
+    ansible-vault encrypt --vault-id ~/askem-vault-id.txt gateway/keycloak/certificates/*.pem
+    ansible-vault encrypt --vault-id ~/askem-vault-id.txt gateway/keycloak/realm/*.json
+}
+
 # Displays the status of all services
 if [[ ${1} == "status" ]]; then
     ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl get po,svc,configMap -n terarium
     exit 0
 fi
 
+if [[ ${1} == "decrypt" ]]; then
+    decrypt
+    exit 0
+fi
+if [[ ${1} == "encrypt" ]]; then
+    encrypt
+    exit 0
+fi
+
 # Launches TERArium
 if [[ ${1} == "up" ]]; then
-    cat namespace.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
-    cat secrets-*.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
-    cat gateway-*.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
-    cat data-*.yaml         |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
-    cat model-*.yaml        |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
-    cat hmi-*.yaml          |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
+    decrypt
+    kubectl kustomize . | ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl apply --filename -
     exit 0
 fi
 
 # Tears down TERArium
 if [[ ${1} == "down" ]]; then
-    cat hmi-*.yaml          |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
-    cat model-*.yaml        |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
-    cat data-*.yaml         |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
-    cat gateway-*.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
-    cat secrets-*.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
-    cat namespace.yaml      |  ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
+    decrypt
+    kubectl kustomize . | ssh uncharted-askem-staging-askem-staging-kube-manager-1 sudo kubectl delete --filename -
     exit 0
 fi
 
