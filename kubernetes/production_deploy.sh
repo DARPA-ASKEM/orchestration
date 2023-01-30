6a7e942b77d0c1608dbbfb882b0cdefe1375bef5
dgauldie <dgauldie@uncharted.software>
2023-01-30 11:12:58 -0500

    fix: fix production deployment scripts

--- kubernetes/production_deploy.sh
+++ kubernetes/production_deploy.sh
@@ -86,17 +86,17 @@ fi
 
 case ${ENVIRONMENT} in
 staging)
-	SECRET_FILES+=("prod/base/gateway/certificates/cert.pem" "prod/base/gateway/certificates/key.pem")
-	SECRET_FILES+=("prod/overlays/askem-staging/secrets/*.yaml")
-	SECRET_FILES+=("prod/base/gateway/keycloak/realm/*.json" "prod/overlays/askem-staging/gateway/keycloak/realm/*.json")
-	KUSTOMIZATION=prod/overlays/askem-staging
+	SECRET_FILES+=("overlays/prod/base/gateway/certificates/cert.pem" "overlays/prod/base/gateway/certificates/key.pem")
+	SECRET_FILES+=("overlays/prod/overlays/askem-staging/secrets/*.yaml")
+	SECRET_FILES+=("overlays/prod/base/gateway/keycloak/realm/*.json" "overlays/prod/overlays/askem-staging/gateway/keycloak/realm/*.json")
+	KUSTOMIZATION=overlays/prod/overlays/askem-staging
 	KUBECTL_CMD="ssh uncharted-askem-prod-askem-staging-kube-manager-1 sudo kubectl"
 	;;
 production)
-	SECRET_FILES+=("prod/base/gateway/certificates/cert.pem" "prod/base/gateway/certificates/key.pem")
-	SECRET_FILES+=("prod/overlays/askem-production/secrets/*.yaml")
-	SECRET_FILES+=("prod/base/gateway/keycloak/realm/*.json" "prod/overlays/askem-production/gateway/keycloak/realm/*.json")
-	KUSTOMIZATION=prod/overlays/askem-production
+	SECRET_FILES+=("overlays/prod/base/gateway/certificates/cert.pem" "overlays/prod/base/gateway/certificates/key.pem")
+	SECRET_FILES+=("overlays/prod/overlays/askem-production/secrets/*.yaml")
+	SECRET_FILES+=("overlays/prod/base/gateway/keycloak/realm/*.json" "overlays/prod/overlays/askem-production/gateway/keycloak/realm/*.json")
+	KUSTOMIZATION=overlays/prod/overlays/askem-production
 	KUBECTL_CMD="ssh uncharted-askem-prod-askem-prod-kube-manager-1 sudo kubectl"
 	;;
 esac
