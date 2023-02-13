114ab3d7ea079746f46520b2c21f4a39ac6957a4
Charles Coleman <bigglesandginger@gmail.com>
2023-02-13 13:34:52 -0500

    add check-latest for aske

--- kubernetes/production_deploy.sh
+++ kubernetes/production_deploy.sh
@@ -95,6 +95,7 @@ staging)
 	SECRET_FILES+=("overlays/prod/base/gateway/certificates/cert.pem" "overlays/prod/base/gateway/certificates/key.pem")
 	SECRET_FILES+=("overlays/prod/overlays/askem-staging/secrets/*.yaml")
 	SECRET_FILES+=("overlays/prod/base/gateway/keycloak/realm/*.json" "overlays/prod/overlays/askem-staging/gateway/keycloak/realm/*.json")
+	SECRET_FILES+=("overlays/prod/overlays/askem-staging/check-latest/check-latest-rsa" "overlays/prod/overlays/askem-staging/check-latest/secrets.yaml")
 	KUSTOMIZATION=overlays/prod/overlays/askem-staging
 	KUBECTL_CMD="ssh uncharted-askem-prod-askem-staging-kube-manager-1 sudo kubectl"
 	;;
