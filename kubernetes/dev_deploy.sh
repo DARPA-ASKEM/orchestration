55079c49f6c3e87afbb134a752f425f0350660ee
David Gauldie <dgauldie@uncharted.software>
2023-05-11 16:14:42 -0400

    chore: 140 rabbit mq available on staging and production (#144)

--- kubernetes/dev_deploy.sh
+++ kubernetes/dev_deploy.sh
@@ -8,7 +8,6 @@ HMI_SERVER_REPLACEMENT_FILE="overlays/dev/local/hmi/server/configmap/host.yaml"
 HMI_SERVER_REPLACEMENT_FILE_MAC="overlays/dev/local/hmi/server/configmap/host-mac.yaml"
 HMI_SERVER_REPLACEMENT_FILE_LINUX="overlays/dev/local/hmi/server/configmap/host-linux.yaml"
 
-
 SECRET_FILES=()
 SECRET_FILES+=("overlays/dev/local/secrets/*.yaml")
 
@@ -20,185 +19,214 @@ determine_host_machine_for_pods() {
 	cp ${HMI_SERVER_REPLACEMENT_FILE_MAC} ${HMI_SERVER_REPLACEMENT_FILE}
 
 	case $(uname) in
-		"Linux")
-			# Linux
-			cp ${GATEWAY_HTTPD_REPLACEMENT_FILE_LINUX} ${GATEWAY_HTTPD_REPLACEMENT_FILE}
-			cp ${HMI_SERVER_REPLACEMENT_FILE_LINUX} ${HMI_SERVER_REPLACEMENT_FILE}
+	"Linux")
+		# Linux
+		cp ${GATEWAY_HTTPD_REPLACEMENT_FILE_LINUX} ${GATEWAY_HTTPD_REPLACEMENT_FILE}
+		cp ${HMI_SERVER_REPLACEMENT_FILE_LINUX} ${HMI_SERVER_REPLACEMENT_FILE}
 
-			echo "You are running Linux"
-			LOCALHOST=$(hostname -I | awk '{print $1}')
-			if [[ "$(< /proc/sys/kernel/osrelease)" == *WSL2 ]];
-			then
-				echo "But on Windows in WSL2 mode...  If there's a problem, this is probably it =)"
-				LOCALHOST="`ip a s eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2`" # Get the IP on eth0
-				echo "Assigning docker host to WSL2 VM IP ${LOCALHOST}"
-			fi
+		echo "You are running Linux"
+		LOCALHOST=$(hostname -I | awk '{print $1}')
+		if [[ "$(</proc/sys/kernel/osrelease)" == *WSL2 ]]; then
+			echo "But on Windows in WSL2 mode...  If there's a problem, this is probably it =)"
+			LOCALHOST="$(ip a s eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)" # Get the IP on eth0
+			echo "Assigning docker host to WSL2 VM IP ${LOCALHOST}"
+		fi
 
-			echo "editing files replacing 'localhost' with ${LOCALHOST}"
+		echo "editing files replacing 'localhost' with ${LOCALHOST}"
 
-			sed -i.bak "s/localhost/${LOCALHOST}/g" ${GATEWAY_HTTPD_REPLACEMENT_FILE}
-			sed -i.bak "s/localhost/${LOCALHOST}/g" ${HMI_SERVER_REPLACEMENT_FILE}
-			;;
+		sed -i.bak "s/localhost/${LOCALHOST}/g" ${GATEWAY_HTTPD_REPLACEMENT_FILE}
+		sed -i.bak "s/localhost/${LOCALHOST}/g" ${HMI_SERVER_REPLACEMENT_FILE}
+		;;
 	esac
 }
 
 case ${1} in
-	-h | --help)
-		COMMAND="help"
-		;;
-	test)
-		COMMAND="test"
-		;;
-	up)
-		COMMAND="up"
-		shift
-		SERVICES=("$@")
-		;;
-	down)
-		COMMAND="down"
-		shift
-		SERVICES=("$@")
-		;;
-	status)
-		COMMAND="status"
-		;;
-	decrypt)
-		COMMAND="decrypt"
-		;;
-	encrypt)
-		COMMAND="encrypt"
-		;;
-	*)
-		echo "dev_deploy.sh: illegal option"
-		;;
+-h | --help)
+	COMMAND="help"
+	;;
+test)
+	COMMAND="test"
+	;;
+up)
+	COMMAND="up"
+	shift
+	SERVICES=("$@")
+	;;
+down)
+	COMMAND="down"
+	shift
+	SERVICES=("$@")
+	;;
+status)
+	COMMAND="status"
+	;;
+decrypt)
+	COMMAND="decrypt"
+	;;
+encrypt)
+	COMMAND="encrypt"
+	;;
+*)
+	echo "dev_deploy.sh: illegal option"
+	;;
 esac
 
 # Default COMMAND to help if empty
 COMMAND=${COMMAND:-help}
 
 case ${COMMAND} in
-	test)
-		echo "## Decrypting secrets"
-		decrypt
-		echo "## Testing kustomization script"
-		kubectl kustomize ./overlays/dev/local | less
-		echo "## Restoring secrets as encrypted files"
-		restore
-		;;
-	up)
-		echo "## Decrypting secrets"
-		decrypt
-		determine_host_machine_for_pods
-		if [ "${#SERVICES[@]}" -eq 0 ]; then
-			echo "Launching TERArium on localhost..."
-			kubectl kustomize ./overlays/dev/local | kubectl apply --filename -
-		else
-			for SERVICE in "${SERVICES[@]}"; do
-				case ${SERVICE} in
-					hmi-client)
-						echo "Launching HMI CLIENT on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/client | kubectl apply --filename -
-						;;
+test)
+	echo "## Decrypting secrets"
+	decrypt
+	echo "## Testing kustomization script"
+	kubectl kustomize ./overlays/dev/local | less
+	echo "## Restoring secrets as encrypted files"
+	restore
+	;;
+up)
+	echo "## Decrypting secrets"
+	decrypt
+	determine_host_machine_for_pods
+	if [ "${#SERVICES[@]}" -eq 0 ]; then
+		echo "Launching TERArium on localhost..."
+		kubectl kustomize ./overlays/dev/local | kubectl apply --filename -
+	else
+		for SERVICE in "${SERVICES[@]}"; do
+			case ${SERVICE} in
+			hmi-client)
+				echo "Launching HMI CLIENT on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi/client | kubectl apply --filename -
+				;;
 
-					hmi-server)
-						echo "Launching HMI SERVER on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/server | kubectl apply --filename -
-						;;
+			hmi-server)
+				echo "Launching HMI SERVER on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi/server | kubectl apply --filename -
+				;;
 
-					hmi-postgres)
-						echo "Launching HMI POSTGRES DB on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/server/postgres | kubectl apply --filename -
-						;;
+			user-store)
+				echo "Launching USER STORE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/user-store | kubectl apply --filename -
+				;;
 
-					data-service)
-						echo "Launching DATA SERVICE on localhost..."
-						kubectl kustomize ./overlays/dev/local/services/data-service | kubectl apply --filename -
-						;;
+			message-queue)
+				echo "Launching MESSAGE QUEUE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/message-queue | kubectl apply --filename -
+				;;
 
-					model-service)
-						echo "Launching MODEL SERVICE on localhost..."
-						kubectl kustomize ./overlays/dev/local/services/model-service | kubectl apply --filename -
-						;;
+			data-service)
+				echo "Launching DATA SERVICE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/data-service | kubectl apply --filename -
+				;;
 
-					gateway)
-						echo "Launching GATEWAY on localhost..."
-						kubectl kustomize ./overlays/dev/local/gateway | kubectl apply --filename -
-						;;
+			model-service)
+				echo "Launching MODEL SERVICE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/model-service | kubectl apply --filename -
+				;;
 
-					*)
-						echo "'${SERVICE}' is not a valid service. Please specify a valid service."
-						;;
-				esac
-			done
-		fi
-		echo "## Restoring secrets as encrypted files"
-		restore
-		;;
-	down)
-		echo "## Decrypting secrets"
-		decrypt
-		determine_host_machine_for_pods
-		if [ "${#SERVICES[@]}" -eq 0 ]; then
-			echo "Launching TERArium on localhost..."
-			kubectl kustomize ./overlays/dev/local | kubectl delete --filename -
-		else
-			for SERVICE in "${SERVICES[@]}"; do
-				case ${SERVICE} in
-					hmi-client)
-						echo "Tearing down HMI CLIENT on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/client | kubectl delete --filename -
-						;;
+			services)
+				echo "Launching SERVICES on localhost..."
+				kubectl kustomize ./overlays/dev/local/services | kubectl apply --filename -
+				;;
 
-					hmi-server)
-						echo "Tearing down HMI SERVER on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/server | kubectl delete --filename -
-						;;
+			hmi)
+				echo "Launching HMI on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi | kubectl apply --filename -
+				;;
 
-					hmi-postgres)
-						echo "Tearing down HMI POSTGRES DB on localhost..."
-						kubectl kustomize ./overlays/dev/local/hmi/server/postgres | kubectl delete --filename -
-						;;
+			gateway)
+				echo "Launching GATEWAY on localhost..."
+				kubectl kustomize ./overlays/dev/local/gateway | kubectl apply --filename -
+				;;
 
-					data-service)
-						echo "Tearing down DATA SERVICE on localhost..."
-						kubectl kustomize ./overlays/dev/local/services/data-service | kubectl delete --filename -
-						;;
+			*)
+				echo "'${SERVICE}' is not a valid service. Please specify a valid service."
+				;;
+			esac
+		done
+	fi
+	echo "## Restoring secrets as encrypted files"
+	restore
+	;;
+down)
+	echo "## Decrypting secrets"
+	decrypt
+	determine_host_machine_for_pods
+	if [ "${#SERVICES[@]}" -eq 0 ]; then
+		echo "Launching TERArium on localhost..."
+		kubectl kustomize ./overlays/dev/local | kubectl delete --filename -
+	else
+		for SERVICE in "${SERVICES[@]}"; do
+			case ${SERVICE} in
+			hmi-client)
+				echo "Tearing down HMI CLIENT on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi/client | kubectl delete --filename -
+				;;
 
-					model-service)
-						echo "Tearing down MODEL SERVICE on localhost..."
-						kubectl kustomize ./overlays/dev/local/services/model-service | kubectl delete --filename -
-						;;
+			hmi-server)
+				echo "Tearing down HMI SERVER on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi/server | kubectl delete --filename -
+				;;
 
-					gateway)
-						echo "Tearing down GATEWAY on localhost..."
-						kubectl kustomize ./overlays/dev/local/gateway | kubectl delete --filename -
-						;;
+			user-store)
+				echo "Tearing down USER STORE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/user-store | kubectl delete --filename -
+				;;
 
-					"")
-						echo "Tearing down TERArium on localhost..."
-						kubectl kustomize ./overlays/dev/local | kubectl delete --filename -
-						;;
+			message-queue)
+				echo "Tearing down MESSAGE QUEUE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/message-queue | kubectl delete --filename -
+				;;
 
-					*)
-						echo "Please specify a valid service or leave blank to launch TERArium."
-						;;
-				esac
-			done
-		fi
-		echo "## Restoring secrets as encrypted files"
-		restore
-		;;
-	status)
-		kubectl get configMap,svc,po
-		;;
-	decrypt)
-		decrypt
-		;;
-	encrypt)
-		encrypt
-		;;
-	help)
-		echo "
+			data-service)
+				echo "Tearing down DATA SERVICE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/data-service | kubectl delete --filename -
+				;;
+
+			model-service)
+				echo "Tearing down MODEL SERVICE on localhost..."
+				kubectl kustomize ./overlays/dev/local/services/model-service | kubectl delete --filename -
+				;;
+
+			services)
+				echo "Tearing down SERVICES on localhost..."
+				kubectl kustomize ./overlays/dev/local/services | kubectl delete --filename -
+				;;
+
+			hmi)
+				echo "Tearing down HMI on localhost..."
+				kubectl kustomize ./overlays/dev/local/hmi | kubectl delete --filename -
+				;;
+
+			gateway)
+				echo "Tearing down GATEWAY on localhost..."
+				kubectl kustomize ./overlays/dev/local/gateway | kubectl delete --filename -
+				;;
+
+			"")
+				echo "Tearing down TERArium on localhost..."
+				kubectl kustomize ./overlays/dev/local | kubectl delete --filename -
+				;;
+
+			*)
+				echo "Please specify a valid service or leave blank to launch TERArium."
+				;;
+			esac
+		done
+	fi
+	echo "## Restoring secrets as encrypted files"
+	restore
+	;;
+status)
+	kubectl get configMap,svc,po
+	;;
+decrypt)
+	decrypt
+	;;
+encrypt)
+	encrypt
+	;;
+help)
+	echo "
 	Usage:
 			${0} status              Displays the status of all services
 			${0} decrypt             Decrypt secrets for editing
@@ -214,5 +242,5 @@ case ${COMMAND} in
 			model-service
 			gateway
 			"
-		;;
+	;;
 esac
