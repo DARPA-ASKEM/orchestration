440bd0e41c044a724dfea2d9605003bc07427dfa
Charles Coleman <bigglesandginger@gmail.com>
2024-02-22 10:58:22 -0500

    secret files refactor

--- kubernetes/functions.sh
+++ kubernetes/functions.sh
@@ -18,35 +18,44 @@ checkPrograms() {
 # SOPS
 decrypt() {
   DECRYPTED_FILES=()
-  for LOCATION in ${SECRET_FILES[@]}; do
-    for SECRET_FILE in `ls -1 ${LOCATION}`; do
-      echo "decrypting file ${SECRET_FILE}"
-      sops --decrypt --in-place ${SECRET_FILE}
-      STATUS=$?
-      if [[ ${STATUS} -eq 0 ]]; then
-        DECRYPTED_FILES+=( ${SECRET_FILE} )
-      fi
-    done
+  for SECRET_FILE in ${SECRET_FILES[@]}; do
+    EXTENSION="${SECRET_FILE##*.}"
+    FILENAME="${SECRET_FILE%.*}"
+    INPUT_FILENAME="${FILENAME}.enc.${EXTENSION}"
+    if [[ ${EXTENSION} == ${FILENAME} ]]; then
+      INPUT_FILENAME="${FILENAME}.enc"
+    fi
+    # echo "decrypting file ${SECRET_FILE}"
+    sops --decrypt ${INPUT_FILENAME} > ${SECRET_FILE}
+    STATUS=$?
+    if [[ ${STATUS} -eq 0 ]]; then
+      DECRYPTED_FILES+=( ${SECRET_FILE} )
+    fi
   done
 }
 
 encrypt() {
   echo ${AGE_PUBLIC_KEY}
-  for LOCATION in ${SECRET_FILES[@]}; do
-    for SECRET_FILE in `ls -1 ${LOCATION}`; do
-      echo "encrypting file ${SECRET_FILE}"
-      if [[ ${SECRET_FILE} == *.yaml ]]; then
-        # YAML
-        sops --age=${AGE_PUBLIC_KEY} --encrypt --encrypted-regex '^(data|stringData)$' --in-place ${SECRET_FILE}
-      else
-        # JSON and other
-        sops --age=${AGE_PUBLIC_KEY} --encrypt --in-place ${SECRET_FILE}
-      fi
-      STATUS=$?
-      if [[ ${STATUS} -eq 0 ]]; then
-        DECRYPTED_FILES+=( ${SECRET_FILE} )
-      fi
-    done
+  for SECRET_FILE in ${SECRET_FILES[@]}; do
+    EXTENSION="${SECRET_FILE##*.}"
+    FILENAME="${SECRET_FILE%.*}"
+    OUTPUT_FILENAME="${FILENAME}.enc.${EXTENSION}"
+    if [[ ${EXTENSION} == ${FILENAME} ]]; then
+      OUTPUT_FILENAME="${FILENAME}.enc"
+    fi
+
+    # echo "encrypting file ${SECRET_FILE}"
+    if [[ ${EXTENSION} == yaml ]]; then
+      # YAML
+      sops --age=${AGE_PUBLIC_KEY} --encrypt --encrypted-regex '^(data|stringData)$' ${SECRET_FILE} > ${OUTPUT_FILENAME}
+    else
+      # JSON and other
+      sops --age=${AGE_PUBLIC_KEY} --encrypt ${SECRET_FILE} > ${OUTPUT_FILENAME}
+    fi
+    STATUS=$?
+    if [[ ${STATUS} -eq 0 ]]; then
+      DECRYPTED_FILES+=( ${SECRET_FILE} )
+    fi
   done
 }
 
--- kubernetes/secret_files.sh
+++ kubernetes/secret_files.sh
@@ -1,9 +1,51 @@
-STAGING_SECRET_FILES=("overlays/prod/base/keycloak/certificates/cert.pem" "overlays/prod/base/keycloak/certificates/key.pem"
-	"overlays/prod/overlays/askem-staging/secrets/*.yaml"
-	"overlays/prod/overlays/askem-staging/keycloak/realm/*.json"
-	"overlays/prod/overlays/askem-staging/check-latest/check-latest-rsa" "overlays/prod/overlays/askem-staging/check-latest/secrets.yaml")
+CERTS=(
+  "overlays/prod/base/keycloak/certificates/cert.pem"
+  "overlays/prod/base/keycloak/certificates/key.pem"
+)
 
-PRODUCTION_SECRET_FILES=("overlays/prod/base/keycloak/certificates/cert.pem" "overlays/prod/base/keycloak/certificates/key.pem"
-	"overlays/prod/overlays/askem-production/secrets/*.yaml"
-	"overlays/prod/overlays/askem-production/keycloak/realm/*.json"
-	"overlays/prod/overlays/askem-production/check-latest/check-latest-rsa" "overlays/prod/overlays/askem-production/check-latest/secrets.yaml")
+STAGING_YAML=(
+  "overlays/prod/overlays/askem-staging/secrets/secrets-adobe-api-key.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-beaker-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-data-service-s3.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-dkg.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-es-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-keycloak-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-logging-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-mit-proxy.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-mq-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-neo4j-auth.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-rds-creds.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-spicedb-shared.yaml"
+  "overlays/prod/overlays/askem-staging/secrets/secrets-xdd-api-key.yaml"
+	"overlays/prod/overlays/askem-staging/check-latest/secrets.yaml"
+)
+STAGING_ADDITIONAL=(
+  "overlays/prod/overlays/askem-staging/keycloak/realm/Uncharted-realm.json"
+  "overlays/prod/overlays/askem-staging/keycloak/realm/Uncharted-users-0.json"
+  "overlays/prod/overlays/askem-staging/check-latest/check-latest-rsa"
+)
+
+PRODUCTION_YAML=(
+  "overlays/prod/overlays/askem-production/secrets/secrets-adobe-api-key.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-beaker-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-data-service-s3.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-dkg.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-es-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-keycloak-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-logging-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-mit-proxy.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-mq-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-neo4j-auth.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-rds-creds.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-spicedb-shared.yaml"
+  "overlays/prod/overlays/askem-production/secrets/secrets-xdd-api-key.yaml"
+	"overlays/prod/overlays/askem-production/check-latest/secrets.yaml"
+)
+PRODUCTION_ADDITIONAL=(
+  "overlays/prod/overlays/askem-production/keycloak/realm/Uncharted-realm.json"
+  "overlays/prod/overlays/askem-production/keycloak/realm/Uncharted-users-0.json"
+  "overlays/prod/overlays/askem-production/check-latest/check-latest-rsa"
+)
+
+STAGING_SECRET_FILES=(${CERTS[@]} ${STAGING_YAML[@]} ${STAGING_ADDITIONAL[@]})
+PRODUCTION_SECRET_FILES=(${CERTS[@]} ${PRODUCTION_YAML[@]} ${PRODUCTION_ADDITIONAL[@]})
