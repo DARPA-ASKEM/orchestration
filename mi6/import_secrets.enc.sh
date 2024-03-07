aabf5e1e1f7571f9a7212a2676849d0c9bd14ea6
Charles Coleman <bigglesandginger@gmail.com>
2024-03-07 10:13:10 -0500

    sops encrypt secrets of mi6

---
+++ mi6/import_secrets.enc.sh
@@ -0,0 +1,20 @@
+{
+	"data": "ENC[AES256_GCM,data:bC/IdOQY4K1vtyHO4MMW5PQfSdFZUJz/FIqFs3lsvc83DDOk4vKzAlRX6QUcpdCis8pzPukMEElymB6dffvrrRKqG30SwfsUQ730NRKNLx0ynbzqEb4bNYYeAiExNDhEi9lN2M8AAuU5CAX0OIy5RUzIUpLwfgagZQ18HAaLdUjMQk85JHGD8VngRbVEG/V1b9+pDO5FEAeqTJ4pRqtuhRWkUhRmr2OqHv+BehZ2YrDrEgCTJG1ZsanZQJwUcWYb0wm1AVUK8ykK2PO2STvVMxrB+Y5Xe3cpCPk/o8maoXINiObS97WLHWELFkD/Ubz16AEY+nzmmXfdrGaEf9ivi20j9W7K/5233pzMxGBkctqd44eOxzupm6VotRN7AcmcWQXsKjEQUebjLVDQ9SjfUWyBB4NO6Av1sJsj5FK+r4zyRBFGaJCaHNbYcwz4kfry2p++QYV81FIHUp6Tbf0ykNqznBC+lEl0i0LoMRnEUC9/37k47guAfCXs5HD3N+zmbtymeC8zGr61YTFxsGGK8V0MVV2mOCL1soDSSQuftjDDIEm3lUMX5nDLh8xj/jgOkymXxCniduAK1erCLkDpIqcEUHoJmK34hpsurFFS3UsfkMoqSuJSWT6ooDklte3qetrZKRTkKKJuDuYjwf3oktkZMMAJcVGXRADgSXZ7eEXPIm7C0ORXtLAIjg==,iv:+eUO80CT5IwdwTn4krucSm1m8elZXVrUQ5FJhyxy0PE=,tag:IiYLFKeIZQ5BhR9Qhj86IA==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": [
+			{
+				"recipient": "age1q5q7lt6twttye7h5dx4zu0eek04pu6szqfx28w00vq3z6nmlc9dq7md4xd",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBUS3UyM0h5NEZLNE1JbmxS\neThXREdBbG5GYVBGWWJoT3dZVmJTL2htc2c0CjMyZnpJNFdLa2RVYzgzWFBNdkJZ\ncU93cGphMXBiZDUrRGNhc3gvM1B6eGMKLS0tIFJaOFBPM0Zmd24rN2l6QjBlVndz\nb1l3MExHUWZuSC92Nzl2UjRFam9kTkEKhXxnrqj9ehbM/7097T4oT30DDoIvScpV\nPJtMcFlhhqeuPNioRCK7C2zrtpL3xzF15W6EyTgoXXnebAn2+k5AVw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2024-03-07T15:12:01Z",
+		"mac": "ENC[AES256_GCM,data:Cn93HwU4RI2hGUmcceq42pSXS7r8HKtFsszr/xeSiCs0NXJOn7Vcj4uWhhwKS1v5OcmBTTcj4S9NvujbjtzS3v8uI1evNY8ivlfC4qu/zZmqy1WFuD+UGWOmqfcTu3YU9JViSjKKb4pPBWgMFPP7nOo5uRWwzAsKuq0aJ819Vx0=,iv:KfQ86nVUiAqglCsgXoVQWZ/1PXW5NOYTOCbc35yjD9U=,tag:ZKs+zQIWqVso7eZYSDnR+g==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
+}
