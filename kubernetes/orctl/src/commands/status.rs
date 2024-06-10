0fcd5da5823467101351bf99069eadab2866c61e
Charles Coleman <bigglesandginger@gmail.com>
2024-06-10 13:34:38 -0400

    wip

--- kubernetes/orctl/src/commands/status.rs
+++ kubernetes/orctl/src/commands/status.rs
@@ -3,29 +3,47 @@ use std::str::Utf8Error;
 use crate::config::verbosity::Verbosity;
 use crate::models::deployment_environment::Environment;
 
-pub(crate) fn get_status(env: Environment, debug_mode: Verbosity, pod: &bool, svc: &bool) -> Result<(), Utf8Error> {
+pub(crate) fn get_status(env: Environment, verbosity: Verbosity, pod: &bool, svc: &bool) -> Result<(), Utf8Error> {
+    if verbosity >= Verbosity::INFO {
+        print!("Status of :");
+    }
+
+    let mut status_of = "".to_owned();
     if *pod {
-        println!("Status pod only");
-    } else {
-        let ssh_cmd_arg = format!("sudo kubectl get pod -o wide");
-        if debug_mode >= Verbosity::INFO {
-            println!("Status to be fetched {}", ssh_cmd_arg);
+        if verbosity >= Verbosity::INFO {
+            print!(" pod");
         }
-        let output = Command::new("ssh")
-            .arg(env.ssh_cmd)
-            .arg(ssh_cmd_arg)
-            .output()
-            .expect("failed to execute");
-        if output.status.success() {
-            let output = std::str::from_utf8(&output.stdout)?;
-            println!("{}", output.to_string());
-        } else {
-            panic!("Error: {}", std::str::from_utf8(&output.stderr)?);
-        }
+        status_of = "pod".to_owned();
     }
 
     if *svc {
-        println!("SVC supplied");
+        if verbosity >= Verbosity::INFO {
+            print!(" svc");
+        }
+        if status_of.len() > 0 {
+            status_of = status_of.to_owned() + ",svc";
+        } else {
+            status_of = "svc".to_owned();
+        }
+    }
+    if verbosity >= Verbosity::INFO {
+        println!("");
+    }
+
+    let ssh_cmd_arg = format!("sudo kubectl -n terarium get {} -o wide", status_of);
+    if verbosity >= Verbosity::INFO {
+        println!("Status to be fetched {}", ssh_cmd_arg);
+    }
+    let output = Command::new("ssh")
+        .arg(env.ssh_cmd)
+        .arg(ssh_cmd_arg)
+        .output()
+        .expect("failed to execute");
+    if output.status.success() {
+        let output = std::str::from_utf8(&output.stdout)?;
+        println!("{}", output.to_string());
+    } else {
+        panic!("Error: {}", std::str::from_utf8(&output.stderr)?);
     }
     return Ok(());
 }
